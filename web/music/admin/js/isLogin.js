b28f979c677daa793d43d91fd9f2814e7b136781
lt199934 <2484202301@qq.com>
2022-10-19 00:50:31 +0800

    后台管理系统优化，添加昵称功能

--- web/music/admin/js/isLogin.js
+++ web/music/admin/js/isLogin.js
@@ -1,16 +1,16 @@
-var user=JSON.parse(sessionStorage.getItem("admin"));
+var user = JSON.parse(sessionStorage.getItem("admin"));
 // console.log(user)
-if(null==user){
- window.location.href="/login";
+if (null == user) {
+    window.location.href = "/admin";
 }
 
-$("#logout").click(function(){
-    var message=confirm("是否退出登录?");
-    if (message==true){
-        var logout=sessionStorage.removeItem("admin");
-        var user=JSON.parse(sessionStorage.getItem("admin"));
+$("#logout").click(function () {
+    var message = confirm("是否退出登录?");
+    if (message == true) {
+        var logout = sessionStorage.removeItem("admin");
+        user = JSON.parse(sessionStorage.getItem("admin"));
         console.log(user);
-        window.location.href="/login";
+        window.location.href = "/admin";
     }
 
 });
