127ad1e53945711b44330be9a94c289643c83d20
lt199934 <2484202301@qq.com>
2023-11-25 19:49:50 +0800

    修复文件上传工具

--- src/main/java/net/ltbk/music/utils/FileHandleUtil.java
+++ src/main/java/net/ltbk/music/utils/FileHandleUtil.java
@@ -1,13 +1,17 @@
 package net.ltbk.music.utils;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+
 /**
  * @Program: music
  * @ClassName File
@@ -16,37 +20,58 @@ import java.io.InputStream;
  * @Create: 2023-03-10 02:01
  * @Version 1.0
  **/
+@Slf4j
+@Component
 public class FileHandleUtil {
 
-    /** 绝对路径 **/
-    private static String absolutePath = "F:/软件开发/项目/SpringBoot+Thymeleaf/music/src/main/resources/";
+    /**
+     * 静态目录
+     **/
+    private static final String STATIC_DIR = "static";
+    /**
+     * 文件存放的目录
+     **/
+    private static final String FILE_DIR = "/upload/";
 
-    /** 静态目录 **/
-    private static String staticDir = "static";
 
-    /** 文件存放的目录 **/
-    private static String fileDir = "/upload/";
+    private static String windowsPath;
+    private static String linuxPath;
+    /**
+     * 绝对路径
+     **/
+    private static String absolutePath;
+
+    @Value("${file.windows.uploadPath}")
+    public void setWindowsPath(String windowsPath) {
+        FileHandleUtil.windowsPath = windowsPath;
+    }
+
+    @Value("${file.linux.uploadPath}")
+    public void setLinuxPath(String linuxPath) {
+        FileHandleUtil.linuxPath = linuxPath;
+    }
 
     /**
      * 上传单个文件
      * 最后文件存放路径为：static/upload/image/test.jpg
      * 文件访问路径为：http://127.0.0.1:8080/upload/image/test.jpg
      * 该方法返回值为：/upload/image/test.jpg
+     *
      * @param inputStream 文件流
-     * @param path 文件路径，如：image/
-     * @param filename 文件名，如：test.jpg
+     * @param path        文件路径，如：image/
+     * @param filename    文件名，如：test.jpg
      * @return 成功：上传后的文件访问路径，失败返回：null
      */
     public static String upload(InputStream inputStream, String path, String filename) {
         //第一次会创建文件夹
         createDirIfNotExists();
-        String filePath = fileDir + path + filename;
+        String filePath = FILE_DIR + path + filename;
         String resultPath = '/' + path + filename;
         //存文件
-        File uploadFile = new File(absolutePath, staticDir + filePath);
-        System.out.println(uploadFile.exists());
+        File uploadFile = new File(absolutePath, STATIC_DIR + filePath);
+        log.info("{}", uploadFile.exists());
         if (uploadFile.exists()) {
-          return resultPath;
+            return resultPath;
         }
         try {
             FileUtils.copyInputStreamToFile(inputStream, uploadFile);
@@ -62,7 +87,10 @@ public class FileHandleUtil {
      * 创建文件夹路径
      */
     private static void createDirIfNotExists() {
-        if (!absolutePath.isEmpty()) {return;}
+        init();
+        if (!absolutePath.isEmpty()) {
+            return;
+        }
 
         //获取跟目录
         File file = null;
@@ -71,28 +99,40 @@ public class FileHandleUtil {
         } catch (FileNotFoundException e) {
             throw new RuntimeException("获取根目录失败，无法创建上传目录！");
         }
-        if(!file.exists()) {
+        if (!file.exists()) {
             file = new File("");
         }
 
         absolutePath = file.getAbsolutePath();
 
-        File upload = new File(absolutePath, staticDir + fileDir);
-        if(!upload.exists()) {
+        File upload = new File(absolutePath, STATIC_DIR + FILE_DIR);
+        if (!upload.exists()) {
             upload.mkdirs();
         }
     }
 
     /**
      * 删除文件
+     *
      * @param path 文件访问的路径upload开始 如： /image/test.jpg
      * @return true 删除成功； false 删除失败
      */
     public static boolean delete(String path) {
-        File file = new File(absolutePath, staticDir + path);
+        File file = new File(absolutePath, STATIC_DIR + path);
         if (file.exists()) {
             return file.delete();
         }
         return false;
+    }
+
+    public static void init() {
+        String os = System.getProperty("os.name");
+        //Windows操作系统
+        if (os != null && os.toLowerCase().startsWith("windows")) {
+            absolutePath = windowsPath;
+        } else if (os != null && os.toLowerCase().startsWith("linux")) {
+            absolutePath = linuxPath;
+        }
+        log.info("绝对路径：{}", absolutePath);
     }
 }
